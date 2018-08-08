bba16ec81c8fd3790139c6c192e45f1b837936af
dbqqbqg <dbqqbq@gmail.com>
2018-08-09 01:42:08 +0600

    Start

---
+++ template/menu.php
@@ -0,0 +1,14 @@
+<?php
+ $menu = <<<MENU
+<link rel="stylesheet" href="../css/header.css">
+<menu class="header">
+  <ul class="menu">
+   <li class="element_menu">LINUX</li>
+   <li class="element_menu">PHP</li>
+   <li class="element_menu">SQL</li>
+   <li class="element_menu">CSS</li>
+ </ul>
+</menu>
+
+ MENU;
+?>
