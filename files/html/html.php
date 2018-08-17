1abf94d767f7fbfd575e8b31d8521f9c779a32ce
dbqqbqg <dbqqbq@gmail.com>
2018-08-18 00:33:39 +0600

    standart_commit

--- files/html/html.php
+++
@@ -1,8 +0,0 @@
-<?php
-require "../../templates/templates.php";
-$title = "HTML5";
-echo KHEAD1;
-echo $title;
-echo KHEAD2;
-echo KFOOT;
-?>
--- files/php/php.php
+++
@@ -1,8 +0,0 @@
-<?php
-require "../../templates/templates.php";
-$title = "PHP7";
-echo KHEAD1;
-echo $title;
-echo KHEAD2;
-echo KFOOT;
-?>
